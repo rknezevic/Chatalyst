1a5e474727a537f95422adb18ece0ca0ccaf15a5
lucija3462 <lb54599@fer.hr>
2025-07-28 10:48:22 +0200

    backend

---
+++ backend/Chatalyst/src/main/java/com/example/Chatalyst/ChatalystApplication.java
@@ -0,0 +1,13 @@
+package com.example.Chatalyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatalystApplication.class, args);
+	}
+
+}
---
+++ backend/Chatalyst/src/test/java/com/example/Chatalyst/ChatalystApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Chatalyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatalystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
