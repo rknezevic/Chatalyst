56836afaf8ea122bcbc0e31ba9c1917c94986a25
lucija3462 <lb54599@fer.hr>
2025-07-29 09:57:03 +0200

    modeli

--- backend/Chatalyst/src/main/java/com/example/Chatalyst/controller/ChatController.java
+++ backend/Chatalyst/src/main/java/com/example/Chatalyst/controller/ChatController.java
@@ -3,6 +3,9 @@ package com.example.Chatalyst.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 
         private final ChatMemory chatMemory;
 
+
         public ChatController(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
             this.chatClient = chatClientBuilder.build();
             this.chatMemory = chatMemory;
         }
+
 
         @PostMapping("/chat")
         public String chat(@RequestBody String input) {
